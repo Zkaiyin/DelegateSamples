7fd2654e99e8fcc6c000a3e26538b3e0a745e2b4
Zkaiyin <zkaiyin8@gmail.com>
2024-05-07 11:56:17 +0800

    加入專案檔案。

---
+++ DelegateSample001/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateSample001
+{
+    public delegate void SomeAction(string message);
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
